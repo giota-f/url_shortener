a202be17145180f28ce311c2ee4e0bc45b37c526
root <root@ip-172-31-0-252.us-east-2.compute.internal>
2019-07-25 21:23:19 +0000

    Add restapi functionality

---
+++ restapi/models.py
@@ -0,0 +1,14 @@
+import string
+from restapi import mysql
+
+class Link(mysql.Model):
+    id = mysql.Column(mysql.Integer, primary_key=True)
+    original_url = mysql.Column(mysql.String(512))
+    short_url = mysql.Column(mysql.String(100), unique=True)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.short_url = self.generate_short_link()
+
+    def generate_short_link(self):
+        pass
--- restapi/views.py
+++ restapi/views.py
@@ -1,6 +1,19 @@
 from restapi import app
+from flask import render_template, request, redirect
+from restapi import mysql
+from .models import Link
 
 ### Endpoints ###
 @app.route('/')
-def hello_world():
-    return 'Hello from application', 200
+def index():
+    return render_template('index.html')
+
+@app.route('/add_link', methods=['POST'])
+def add_link():
+    original_url = request.form['original_url']
+    link = Link(original_url=original_url)
+    mysql.session.add(link)
+    mysql.session.commit()
+
+    return render_template('link_added.html',
+        new_link=link.short_url, original_url=link.original_url)
